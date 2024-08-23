0f89aa6c5b97f79718f92d259423683430d71c7c
rafael <sta_rafinha@hotmail.com>
2024-08-23 15:20:16 -0300

    feat: exemplo lista genérica

---
+++ src/main/java/br/com/comparablexcomparator/Box.java
@@ -0,0 +1,15 @@
+package br.com.comparablexcomparator;
+// Lista genérica
+public class Box<T> {
+
+    private T t;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+}
+
---
+++ src/main/java/br/com/comparablexcomparator/Program.java
@@ -0,0 +1,14 @@
+package br.com.comparablexcomparator;
+
+public class Program  {
+
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<>();
+        integerBox.setT(123);
+        System.out.println(integerBox);
+        Integer interValue = integerBox.getT();
+        System.out.println(interValue);
+
+    }
+
+}
